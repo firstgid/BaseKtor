3430f356120d9e95710468c42e50764734327329
Ograniczony <mcjdzi@gmail.com>
2017-12-26 04:13:48 +0100

    first commit

---
+++ src/main/kotlin/blog/BlogApp.kt
@@ -0,0 +1,24 @@
+package blog
+
+import io.ktor.application.*
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun Application.main() {
+	install(DefaultHeaders)
+	install(CallLogging)
+	install(Routing) {
+		get("/") {
+			call.respondText("Hello World Two", ContentType.Text.Html)
+		}
+	}
+}
+
+//fun main(args: Array<String>) {
+//	embeddedServer(Netty, 8080, watchPaths = listOf("BlogApp.kt"), module = Application::module).start()
+//}
